cfaf065f8a4ab9832672a889a2640dac51e89d35
Giousa <65489469@qq.com>
2017-02-12 15:25:33 +0800

    map和it，打印迭代器

---
+++ app/src/main/java/com/giousa/kotlinpractice/ct03/rename.kt
@@ -0,0 +1,20 @@
+package com.giousa.kotlinpractice.ct03
+
+/**
+ *
+ * Description: 可以去除out
+ * Author:zhangmengmeng
+ * Date:2017/2/12
+ * Time:下午3:20
+ *
+ * map:扩展方法
+ * it:迭代器
+ * 测试时,需要在上面运行的三角按钮下的Edit下,加入几个参数
+ *
+ */
+
+fun main(args: Array<String>){
+    args.map {
+        println(it)
+    }
+}
